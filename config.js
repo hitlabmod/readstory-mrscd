aa401ab33f51398e1229d5251951fc6e1b5e3bde
hitlabmod <hitlabmod@gmail.com>
2025-03-10 00:28:30 +0000

    Update files

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
+// Catatan: Setelah mengubah nilai true atau false, restart panel untuk mengaktifkan fitur.
+
+// -------------------- Pengaturan Umum --------------------
 export default {
 	// Atur ke true untuk mengaktifkan auto online, atau false untuk menonaktifkan
 	autoOnline: true,
+
+	// -------------------- Pengaturan Fitur --------------------
 	// Atur ke true untuk mengaktifkan auto typing, atau false untuk menonaktifkan
 	autoTyping: true,
 	// Atur ke true untuk mengaktifkan auto record, atau false untuk menonaktifkan
 	autoRecord: false,
+
+	// -------------------- Pengaturan Lainnya --------------------
 	// ...konfigurasi lainnya...
 };
